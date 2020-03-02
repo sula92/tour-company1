3e3f36e5b018fc9d2bb56f65f9b196bb012c42fe
sula92 <57085468+sula92@users.noreply.github.com>
2020-03-02 09:52:04 -0800

    first

---
+++ myscript2.js
@@ -0,0 +1,49 @@
+$(".hide1").hide();
+
+function func() {
+    $("#btn1").show();
+}
+
+function func2() {
+    $("#btn1").hide();
+}
+
+function func3() {
+    $("#btn2").show();
+}
+
+function func4() {
+    $("#btn2").hide();
+}
+
+function func5() {
+    $("#btn3").show();
+}
+
+function func6() {
+    $("#btn3").hide();
+}
+
+function func7() {
+    $("#btn4").show();
+}
+
+function func8() {
+    $("#btn4").hide();
+}
+
+function func9() {
+    $("#btn5").show();
+}
+
+function func10() {
+    $("#btn5").hide();
+}
+
+function func11() {
+    $("#btn6").show();
+}
+
+function func12() {
+    $("#btn6").hide();
+}
