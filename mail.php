c722fe60bf751de9264cfbe09b4a6e8f470796cc
sula92 <57085468+sula92@users.noreply.github.com>
2020-03-07 21:02:22 -0800

    mail.php added

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST["SubmitBtn"])){
+
+$to = "sulakkanaid@gmail.com";
+$nm = $_POST["nm"];
+$from=$_POST["email"];
+$msg=$_POST["msg"];
+$headers = "From: $from";
+
+mail($to,$subject,$msg,$headers);
+echo "Email successfully sent.";
+}
+?>
